cf25efb317cc546e350aafad41c894c2a9cf6d24
Purva <purvarani.srk@cognizant.com>
2023-06-15 11:44:19 +0530

    Pagination changes in Table Search component

--- react_tasks/src/table_search/TableSearch.js
+++ react_tasks/src/table_search/TableSearch.js
@@ -7,12 +7,25 @@ const TableSearch = () => {
   const recordsPerPage = 5;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  const dataRecords = Users.slice(firstIndex, lastIndex);
-  const noOfPages = Math.ceil(Users.length / recordsPerPage);
-  const numbers = [...Array(noOfPages + 1).keys()].slice(1);
+  const filteredDataRecords = Users.filter(
+    (user) =>
+      user.first_name.includes(search) ||
+      user.first_name.toLowerCase().includes(search) ||
+      user.first_name.toUpperCase().includes(search) ||
+      user.last_name.includes(search) ||
+      user.last_name.toLowerCase().includes(search) ||
+      user.last_name.toUpperCase().includes(search) ||
+      user.email.includes(search) ||
+      user.email.toLowerCase().includes(search) ||
+      user.email.toUpperCase().includes(search)
+  );
+  const dataRecords = filteredDataRecords.slice(firstIndex, lastIndex);
+  const noOfPages = Math.ceil(filteredDataRecords.length / recordsPerPage);
+  const pageNumbers = [...Array(noOfPages + 1).keys()].slice(1);
 
   const handleSearchInput = (e) => {
     setSearch(e.target.value);
+    setCurrentPage(1);
   };
 
   const handlePrevPage = () => {
@@ -34,14 +47,15 @@ const TableSearch = () => {
   return (
     <div>
       <input type="text" placeholder="Search..." onChange={handleSearchInput} />
-      {search === "" ? (
-        <table>
+      <table>
+        <thead>
           <tr>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
           </tr>
-
+        </thead>
+        <tbody>
           {dataRecords.map((user) => (
             <tr key={user.id}>
               <td>{user.first_name}</td>
@@ -49,58 +63,30 @@ const TableSearch = () => {
               <td>{user.email}</td>
             </tr>
           ))}
-        </table>
-      ) : (
-        <table>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-
-          {Users.filter(
-            (user) =>
-              user.first_name.includes(search) ||
-              user.first_name.toLowerCase().includes(search) ||
-              user.first_name.toUpperCase().includes(search) ||
-              user.last_name.includes(search) ||
-              user.last_name.toLowerCase().includes(search) ||
-              user.last_name.toUpperCase().includes(search) ||
-              user.email.includes(search) ||
-              user.email.toLowerCase().includes(search) ||
-              user.email.toUpperCase().includes(search)
-          ).map((user) => (
-            <tr key={user.id}>
-              <td>{user.first_name}</td>
-              <td>{user.last_name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </table>
-      )}
-      {search === "" && (
-        <nav className="table-search-flex">
-          <ul className="pagination">
-            <li>
-              <a href="#" onClick={handlePrevPage}>
-                {"<<Prev"}
-              </a>
-            </li>
-            {numbers.map((n, i) => (
-              <li key={i}>
-                <a href="#" onClick={() => changeCurrentPage(n)}>
-                  {n}
-                </a>
-              </li>
-            ))}
-            <li>
-              <a href="#" onClick={handleNextPage}>
-                {"Next>>"}
-              </a>
-            </li>
-          </ul>
-        </nav>
-      )}
+        </tbody>
+      </table>
+      <ul className="pagination">
+        <li>
+          <a disabled={currentPage === 1} onClick={handlePrevPage}>
+            {"<<Prev"}
+          </a>
+        </li>
+        {pageNumbers.map((n, i) => (
+          <li key={i}>
+            <a
+              className={currentPage === n ? "active" : ""}
+              onClick={() => changeCurrentPage(n)}
+            >
+              {n}
+            </a>
+          </li>
+        ))}
+        <li>
+          <a disabled={currentPage === noOfPages} onClick={handleNextPage}>
+            {"Next>>"}
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
